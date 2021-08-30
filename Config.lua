589c8194e547b693db01b1abc619894e2c15f967
KarpoVelho <40266114+KarpoVelho@users.noreply.github.com>
2021-08-30 20:37:53 +0300

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.paljonpesee = 1000 --kuinka paljon pesee
+Config.paljonantaa = 750 --paljonko antaa jokakerta kun pesee.
+Config.kuinusein = 10 --sekunteina, eli nyt pesee 1000e joka 10sec
+Config.kytat = 0 --kuinka monta kyttää servul pitää olla et alottaa
